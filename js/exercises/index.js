0a32a2785fd989df441d1b5cbc1865b6627c68b0
leizfa <leizfa@gmail.com>
2026-03-13 18:12:05 -0300

    Fix import statements in index.js

--- js/exercises/index.js
+++ js/exercises/index.js
@@ -1,6 +1,6 @@
 // js/exercises/index.js
-import { runbuildBlocks } from "./buildBlocks.js";
-import { runSwapIt } from "./swapIt.js";
+import { runbuildBlocks } from "/buildBlocks.js";
+import { runSwapIt } from "/swapIt.js";
 
 export const exerciseRegistry = {
   buildBlocks: runBuildBlocks,
